01f49095a6a44761afd596edef86ce8d500f6ac2
T-yama0921 <yamata.0921@icloud.com>
2021-06-09 01:21:54 +0900

    新規投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,28 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def tweet_params
+  params.require(:tweet).permit(
+    :question,
+    :answer,
+    :first_incorrection,
+    :second_incorrection,
+    :answer_feedback,
+    :first_feedback,
+    :second_feedback,
+  ).merge(
+    user_id: current_user.id
+  )
 end
