0ba803cf25583669759c87c5b09991403c2608fe
T-yama0921 <yamata.0921@icloud.com>
2021-06-18 01:38:55 +0900

    FactoryBot修正
    StringではChromeで認識できない文字があると思われる。

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :tweet do
-    question            { Faker::String.random }
-    answer              { Faker::String.random }
-    first_incorrection  { Faker::String.random }
-    second_incorrection { Faker::String.random }
-    answer_feedback     { Faker::String.random }
-    first_feedback      { Faker::String.random }
-    second_feedback     { Faker::String.random }
+    question            { Faker::Lorem.question }
+    answer              { Faker::Lorem.sentence }
+    first_incorrection  { Faker::Lorem.sentence }
+    second_incorrection { Faker::Lorem.sentence }
+    answer_feedback     { Faker::Lorem.paragraph }
+    first_feedback      { Faker::Lorem.paragraph }
+    second_feedback     { Faker::Lorem.paragraph }
     association :user
   end
 end
