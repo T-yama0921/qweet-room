0ac2e5ab0feb6ac29d08bc50fad790893df36fcc
T-yama0921 <yamata.0921@icloud.com>
2021-07-06 13:20:43 +0900

    コントローラー定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,6 +43,10 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+
   private
 
   def tweet_params
