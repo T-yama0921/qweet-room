227b62bd0e708b320ad5bebdce741683f6612cd4
T-yama0921 <yamata.0921@icloud.com>
2021-07-06 13:18:56 +0900

    検索メソッドを定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,14 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+
   with_options presence: true do
     validates :question
     validates :answer
